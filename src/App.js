52fd79e1f8efe1927293deed04ceb16ee5c99cd7
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2022-09-11 13:09:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './component/header/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='intro-content'>
+        <img src='/images/Img1.png' alt='img' className='img1'/>
+
+          <div className='intro1-container'>
+            <div className='intro1-content1'>32000</div>
+            <div className='intro1-content2'>kms</div>
+            <p className='intro1-content3'>Highways & Expressway are being supervised and built in order to boost the country's economic development</p>
+          </div>
+
+          <div className='intro2-container'>
+            <img src='/images/Img2.png' alt='img'className='img2'/>
+            <p className='intro2-content1'>Experienced & Expert in Construction.</p>
+          </div>
+
+          <div className='intro3-container'>
+            <i class="fa fa-dot-circle-o"></i>
+            <i class="fa fa-circle-o"></i>
+            <i class="fa fa-circle-o"></i>
+            <i class="fa fa-circle-o"></i>
+          </div>
+
+          <div className='intro4-container'>
+            <i class="fa fa-arrow-down" style={{fontSize:"24px"}}></i>
+          </div>
+
+          <div className='intro5-container'>
+          </div>
+
+      </div>
     </div>
   );
 }
